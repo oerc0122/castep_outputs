90f440496b09c944a02b80b1759b3078cf25bec2
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-02-21 16:10:55 +0000

    Add md test data

--- castep_outputs/test/gen_data.py
+++ castep_outputs/test/gen_data.py
@@ -3,13 +3,20 @@ import pathlib
 
 from castep_outputs.cli.castep_outputs_main import parse_all
 
-for typ in ('castep', 'bands', 'cell', 'param', 'elastic', 'md', 'ts', 'efield',
-            'den_fmt', 'chdiff_fmt', 'pot_fmt', 'elf_fmt', 'xrd_sf', 'phonon_dos',
-            'magres', 'efield', 'tddft', 'err', 'phonon'):
+for type_ in ('castep', 'bands', 'cell', 'param', 'elastic', 'md', 'ts', 'efield',
+              'den_fmt', 'chdiff_fmt', 'pot_fmt', 'elf_fmt', 'xrd_sf', 'phonon_dos',
+              'magres', 'efield', 'tddft', 'err', ('pp-md', 'castep')):
     for fmt in ('json', 'yaml'):
-        print(typ, fmt)
+        print(type_, fmt)
+        if isinstance(type_, tuple):
+            name, typ = type_
+            in_name = name
+        else:
+            name = typ = type_
+            in_name = "test"
+
         # Delete existing
-        pth = pathlib.Path(f"{typ}.{fmt}")
+        pth = pathlib.Path(f"{name}.{fmt}")
         if pth.exists():
             pth.unlink()
-        parse_all(output=str(pth), out_format=fmt, **{typ: [f"test.{typ}"]})
+        parse_all(output=str(pth), out_format=fmt, **{typ: [f"{in_name}.{typ}"]})
