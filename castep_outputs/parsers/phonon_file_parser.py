d64bb7b62933736833fd925f9dafa39b914ba892
KD Brown <kdb516@luna.its.york.ac.uk>
2024-07-23 15:47:12 +0100

    removed empty key and reformatted qpoint eigenvectors into dicts

--- castep_outputs/parsers/phonon_file_parser.py
+++ castep_outputs/parsers/phonon_file_parser.py
@@ -25,6 +25,7 @@ def parse_phonon_file(phonon_file: TextIO) -> Dict[str, Any]:
 
             data = parse_regular_header(block)
             phonon_info.update(data)
+            phonon_info.pop("",None)
             eigenvectors_endblock = (format(phonon_info["branches"], ">4")) + (format(phonon_info["ions"], ">4"))
 
         elif block := get_block(line, phonon_file, "q-pt", "Phonon Eigenvectors", out_fmt=list):
@@ -56,10 +57,11 @@ def parse_phonon_file(phonon_file: TextIO) -> Dict[str, Any]:
                     
                     qdata = {'evec':vectors}
                     fix_data_types(qdata, {'evec': float})
-                    qdata['evec'] = [[qdata['evec'][i], qdata['evec'][i+1]]for i in range(0,len(vectors),2)]
+                    qdata['evec'] = [{"real":qdata['evec'][i], "imag":qdata['evec'][i+1]}for i in range(0,len(vectors),2)]
                     evecs.append(qdata['evec'])
 
                     if len(evecs) == phonon_info["branches"]*phonon_info["ions"]:
                         phonon_info["evecs"].append(evecs)
+                        evecs = []
     
     return phonon_info
