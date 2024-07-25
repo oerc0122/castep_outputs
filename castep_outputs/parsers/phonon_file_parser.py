48877a565fed5e25d5692e766e36a0373c039385
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-07-25 14:54:09 +0100

    Update .phonon parser to return all information from file

--- castep_outputs/parsers/phonon_file_parser.py
+++ castep_outputs/parsers/phonon_file_parser.py
@@ -1,68 +1,93 @@
 """
 Parse the following castep outputs:
-.phonon
+
+- .phonon
 """
 from collections import defaultdict
-from typing import Any, Dict, TextIO
+from typing import TextIO
 
+from ..utilities.datatypes import PhononFileInfo, PhononFileQPoint
 from ..utilities.filewrapper import Block
-from ..utilities.utility import fix_data_types, log_factory
+from ..utilities.utility import log_factory, to_type
 from .parse_utilities import parse_regular_header
 
 
-def parse_phonon_file(phonon_file: TextIO) -> Dict[str, Any]:
-    """ Parse castep .phonon file """
+def parse_phonon_file(phonon_file: TextIO) -> PhononFileInfo:
+    """Parse castep .phonon file.
+
+    Parameters
+    ----------
+    phonon_file
+        A handle to a CASTEP .phonon file.
+
+    Returns
+    -------
+    PhononFileInfo
+        Parsed data.
+    """
     # pylint: disable=too-many-locals
+
     logger = log_factory(phonon_file)
-    phonon_info = defaultdict(list)
-    evals = []
-    evecs = []
+    phonon_info: PhononFileInfo = defaultdict(list)
 
     for line in phonon_file:
         if block := Block.from_re(line, phonon_file, "BEGIN header", "END header"):
 
             data = parse_regular_header(block)
             phonon_info.update(data)
-            phonon_info.pop("", None)
+            del phonon_info[""]
             eigenvectors_endblock = (format(phonon_info["branches"], ">4") +
                                      format(phonon_info["ions"], ">4"))
 
         elif block := Block.from_re(line, phonon_file, "q-pt", "Phonon Eigenvectors"):
 
             logger("Found eigenvalue block")
-            for line in block:
-                if "q-pt" in line:
-                    _, _, posx, posy, posz, *weight = line.split()
-                    qdata = {'pos': [posx, posy, posz], 'weight': weight}
-                    fix_data_types(qdata, {'pos': float,
-                                           'weight': float})
-                    phonon_info["qpt_pos"].append(qdata['pos'])
 
-                elif "Eigenvectors" not in line:
-                    _, e_val, *_ = line.split()
-                    qdata = {'eval': e_val}
-                    fix_data_types(qdata, {'eval': float})
-                    evals.append(qdata['eval'])
-                    if len(evals) == phonon_info["branches"]:
-                        phonon_info["evals"].append(evals)
-                        evals = []
+            _, _, posx, posy, posz, weight, *qpoint_dir = line.split()
+            qdata: PhononFileQPoint = defaultdict(list)
+
+            qdata["qpt"] = to_type((posx, posy, posz), float)
+            qdata["weight"] = float(weight)
+
+            if qpoint_dir:
+                qdata["dir"] = to_type(qpoint_dir, float)
+
+            if len(block) - 2 != phonon_info["branches"]:
+                raise IOError("Malformed phonon frequencies. "
+                              f"Expected {phonon_info['branches']} branches, "
+                              f"received {len(block)-2}")
+
+            block.remove_bounds(1, 1)
+
+            for line in block:
+                # Line columns taken from phonon.f90
+                eigenvalue, ir_intensity, raman_intensity = (line[8:23],
+                                                             line[31:46],
+                                                             line[55:70])
+                qdata["eigenvalues"].append(float(eigenvalue))
+                if ir_intensity.strip():
+                    qdata["ir_intensity"].append(float(ir_intensity))
+                if raman_intensity.strip():
+                    qdata["raman_intensity"].append(float(raman_intensity))
 
         elif block := Block.from_re(line, phonon_file, "Mode Ion", eigenvectors_endblock):
 
             logger("Found eigenvector block")
+
+            total_samp = phonon_info["branches"] * phonon_info["ions"]
+            if len(block) - 1 != total_samp:
+                raise IOError("Malformed eigenvectors. "
+                              f"Expected {total_samp} branches, "
+                              f"received {len(block)-1}")
+
+            block.remove_bounds(1, 0)
+
             for line in block:
-                if "Mode" not in line:
-                    _, _, *vectors = line.split()
+                _, _, *eigenvectors = line.split()
+                eigenvectors = to_type(eigenvectors, float)
+                eigenvectors = map(complex, eigenvectors[::2], eigenvectors[1::2])
+                qdata["eigenvectors"].append(tuple(eigenvectors))
 
-                    qdata = {'evec': vectors}
-                    fix_data_types(qdata, {'evec': float})
-                    qdata['evec'] = [complex(qdata['evec'][i],
-                                             qdata['evec'][i+1])
-                                     for i in range(0, len(vectors), 2)]
-                    evecs.append(qdata['evec'])
-
-                    if len(evecs) == phonon_info["branches"]*phonon_info["ions"]:
-                        phonon_info["evecs"].append(evecs)
-                        evecs = []
+            phonon_info["qpts"].append(qdata)
 
     return phonon_info
