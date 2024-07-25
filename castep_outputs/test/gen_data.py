e700688a3a0e2e8b376a699b7e72ccf8ac1d73eb
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-07-25 11:34:19 +0100

    Regenerate data with ruamel

--- castep_outputs/test/gen_data.py
+++ castep_outputs/test/gen_data.py
@@ -5,7 +5,7 @@ from castep_outputs.cli.castep_outputs_main import parse_all
 
 for typ in ('castep', 'bands', 'cell', 'param', 'elastic', 'md', 'ts', 'efield',
             'den_fmt', 'chdiff_fmt', 'pot_fmt', 'elf_fmt', 'xrd_sf', 'phonon_dos',
-            'magres', 'efield', 'tddft', 'err'):
+            'magres', 'efield', 'tddft', 'err', 'phonon'):
     for fmt in ('json', 'yaml'):
         print(typ, fmt)
         # Delete existing
