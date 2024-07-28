30a13066deef0b0a16e952cd1124e140b19ccca6
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-07-29 00:01:58 +0100

    Refactor efield parser

--- castep_outputs/parsers/efield_file_parser.py
+++ castep_outputs/parsers/efield_file_parser.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import re
 from collections import defaultdict
-from typing import TextIO
+from typing import TextIO, TypedDict
 
 from ..utilities import castep_res as REs
 from ..utilities.castep_res import labelled_floats
@@ -13,23 +13,52 @@ from ..utilities.utility import fix_data_types, log_factory, stack_dict
 from .parse_utilities import parse_regular_header
 
 
-def parse_efield_file(efield_file: TextIO) -> dict[str, float | int]:
+class EFieldTensor(TypedDict):
+    """Standard efield tensor of Voigt components + frequency."""
+
+    xx: list[float]
+    yy: list[float]
+    zz: list[float]
+    xy: list[float]
+    xz: list[float]
+    yz: list[float]
+    freq: list[float]
+
+
+class EFieldInfo(TypedDict, total=False):
+    """Electronic field response information."""
+
+    #: Number of ions in system.
+    ions: int
+    #: Number of phonon branches.
+    branches: int
+    #: Number of frequencies.
+    frequencies: int
+    #: Oscillator Q.?
+    oscillator_Q: list[float]
+    #: Oscillator strengths in (D/A)**2 / amu.
+    oscillator_strengths: EFieldTensor
+    #: Electrical permittivity.
+    permittivity: EFieldTensor
+
+
+def parse_efield_file(efield_file: TextIO) -> EFieldInfo:
     """
-    Parse castep .efield files.
+    Parse castep .efield file.
 
     Parameters
     ----------
-    efield_file
+    efield_file : ~typing.TextIO
         Open handle to file to parse.
 
     Returns
     -------
-    dict[str, float | int]
+    EFieldInfo
         Parsed info.
     """
     # pylint: disable=too-many-branches,redefined-outer-name
 
-    efield_info = defaultdict(list)
+    efield_info: EFieldInfo = defaultdict(list)
     logger = log_factory(efield_file)
 
     for line in efield_file:
@@ -46,12 +75,12 @@ def parse_efield_file(efield_file: TextIO) -> dict[str, float | int]:
             block.remove_bounds(1, 2)
             for line in block:
                 match = re.match(rf"\s*(?P<freq>{REs.INTNUMBER_RE})" +
-                                 labelled_floats([*(f"S{d}" for d in SND_D)]), line)
+                                 labelled_floats(SND_D), line)
                 stack_dict(osc, match.groupdict())
 
             if osc:
                 fix_data_types(osc, {"freq": float,
-                                     **{f"S{d}": float for d in SND_D}})
+                                     **{d: float for d in SND_D}})
                 efield_info["oscillator_strengths"].append(osc)
 
         elif block := Block.from_re(line, efield_file, "BEGIN permittivity", "END permittivity"):
@@ -61,13 +90,12 @@ def parse_efield_file(efield_file: TextIO) -> dict[str, float | int]:
             perm = defaultdict(list)
             block.remove_bounds(1, 2)
             for line in block:
-                match = re.match(labelled_floats(["freq",
-                                                  *(f"e_r_{d}" for d in SND_D)]), line)
+                match = re.match(labelled_floats(["freq", *SND_D]), line)
                 stack_dict(perm, match.groupdict())
 
             if perm:
                 fix_data_types(perm, {"freq": float,
-                                      **{f"e_r_{d}": float for d in SND_D}})
+                                      **{d: float for d in SND_D}})
                 efield_info["permittivity"].append(perm)
 
     return efield_info
