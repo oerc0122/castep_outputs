1d55cb1da397550aad2e4082a9553edf86b22ee9
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-07-29 00:10:59 +0100

    Refactor *_fmt file parsers

--- castep_outputs/parsers/parse_utilities.py
+++ castep_outputs/parsers/parse_utilities.py
@@ -5,7 +5,7 @@ Functions generally used in parsing castep files
 
 import re
 from collections import defaultdict
-from typing import Dict, List, Sequence, TextIO, Tuple, Union
+from typing import Dict, Sequence, Union
 
 from ..utilities import castep_res as REs
 from ..utilities.castep_res import get_numbers
@@ -45,28 +45,3 @@ def parse_regular_header(block: Block,
                             'mass': float})
     data['coords'] = coords
     return data
-
-
-def parse_kpt_info(inp: TextIO, prop: Union[str, Tuple[str]]) -> Dict[str, List[Union[int, float]]]:
-    """ Parse standard form of kpt related .*_fmt files """
-
-    # Skip header
-    while "END header" not in inp.readline():
-        pass
-
-    qdata = defaultdict(list)
-    for line in inp:
-        if not line.strip():
-            continue
-        if isinstance(prop, str):
-            *qpt, val = line.split()
-            qpt = to_type(qpt, int)
-            val = to_type(val, float)
-            stack_dict(qdata, {'q': qpt, prop: val})
-        elif isinstance(prop, tuple):
-            words = line.split()
-            qpt = to_type(words[0:3], int)
-            val = to_type(words[3:], float)
-            stack_dict(qdata, {'q': qpt, **dict(zip(prop, val))})
-
-    return qdata
