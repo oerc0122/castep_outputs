abd3469e46997dd07f5f3c7d9169f32156b61756
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2024-09-06 11:06:34 +0100

    Refactor *_fmt file parsers

--- castep_outputs/parsers/parse_fmt_files.py
+++ castep_outputs/parsers/parse_fmt_files.py
@@ -1,70 +1,166 @@
-"""Parse castep .elf_fmt, .chdiff_fmt, .pot_fmt, and .den_fmt files."""
+"""
+Parse the following castep outputs.
+
+- .elf_fmt
+- .chdiff_fmt
+- .pot_fmt
+- .den_fmt
+"""
 from __future__ import annotations
 
-from typing import TextIO
+import re
+from collections import defaultdict
+from typing import TextIO, TypedDict, cast
 
-from .parse_utilities import parse_kpt_info
+from ..utilities.utility import file_or_path, stack_dict, to_type
 
 
-def parse_elf_fmt_file(elf_file: TextIO) -> dict[str, list[int | float]]:
-    """Parse castep .elf_fmt files.
+class ElfFileInfo(TypedDict, total=False):
+    """Electron localisation function information."""
+
+    #: :math:`q`-point sample.
+    q: list[tuple[int, int, int]]
+    #: Spin-split alpha channel localisation
+    chi_alpha: tuple[float, ...]
+    #: Spin-split beta channel localisation
+    chi_beta: tuple[float, ...]
+    #: Electron localisation function
+    chi: tuple[float, ...]
+
+
+class ChDiffFileInfo(TypedDict):
+    """Charge difference information."""
+
+    #: :math:`q`-point sample.
+    q: list[tuple[int, int, int]]
+    #: Difference between charge density and superposition of atomic densities.
+    charge: tuple[float, ...]
+
+
+class DenFileInfo(TypedDict):
+    """Charge density information."""
+
+    #: :math:`q`-point sample.
+    q: list[tuple[int, int, int]]
+    #: Charge density at point.
+    charge: tuple[float, ...]
+
+
+class PotFileInfo(TypedDict):
+    """Potential information."""
+
+    #: :math:`q`-point sample.
+    q: list[tuple[int, int, int]]
+    #: Charge density at point.
+    pot: tuple[float, ...]
+
+
+@file_or_path(mode="r")
+def _parse_kpt_info(
+        inp: TextIO,
+) -> dict[str, list[int | float]]:
+    """
+    Parse standard form of kpt related .*_fmt files.
 
     Parameters
     ----------
-    elf_file : TextIO
+    inp : TextIO
         File to parse.
 
     Returns
     -------
-    dict[str, list[int | float]]
+    Dict[str, List[Union[int, float]]]
         Parsed data.
     """
-    return parse_kpt_info(elf_file, ("chi_alpha", "chi_beta"))
+    # Skip header
+    while "END header" not in (line := inp.readline()):
+        pass
+
+    cols = re.search(r'"([^"]+)"', line).group(1)
+    prop = tuple(cols.split()[3:]) if len(cols) > 3 else cols[3]
+
+    qdata = defaultdict(list)
+
+    for line in inp:
+        if not line.strip():
+            continue
+        if isinstance(prop, str):
+            *qpt, val = line.split()
+            qpt = to_type(qpt, int)
+            val = to_type(val, float)
+            stack_dict(qdata, {"q": qpt, prop: val})
+        elif isinstance(prop, tuple):
+            words = line.split()
+            qpt = to_type(words[0:3], int)
+            val = to_type(words[3:], float)
+            stack_dict(qdata, {"q": qpt, **dict(zip(prop, val))})
+
+    return qdata
 
 
-def parse_chdiff_fmt_file(chdiff_file: TextIO) -> dict[str, list[int | float]]:
-    """Parse castep .chdiff_fmt files.
+def parse_elf_fmt_file(elf_file: TextIO) -> ElfFileInfo:
+    """
+    Parse castep .elf_fmt file.
 
     Parameters
     ----------
-    chdiff_file : TextIO
-        File to parse.
+    elf_file : ~typing.TextIO
+        Open handle to file to parse.
 
     Returns
     -------
-    dict[str, list[int | float]]
-        Parsed data.
+    ElfFileInfo
+        Parsed info.
     """
-    return parse_kpt_info(chdiff_file, "chdiff")
+    return cast(ElfFileInfo, _parse_kpt_info(elf_file))
 
 
-def parse_pot_fmt_file(pot_file: TextIO) -> dict[str, list[int | float]]:
-    """Parse castep .pot_fmt files.
+def parse_chdiff_fmt_file(chdiff_file: TextIO) -> ChDiffFileInfo:
+    """
+    Parse castep .chdiff_fmt file.
 
     Parameters
     ----------
-    pot_file : TextIO
-        File to parse.
+    chdiff_file : ~typing.TextIO
+        Open handle to file to parse.
 
     Returns
     -------
-    dict[str, list[int | float]]
-        Parsed data.
+    ChDiffFileInfo
+        Parsed info.
     """
-    return parse_kpt_info(pot_file, "pot")
+    return cast(ChDiffFileInfo, _parse_kpt_info(chdiff_file))
 
 
-def parse_den_fmt_file(den_file: TextIO) -> dict[str, list[int | float]]:
-    """Parse castep .den_fmt files.
+def parse_pot_fmt_file(pot_file: TextIO) -> PotFileInfo:
+    """
+    Parse castep .pot_fmt file.
 
     Parameters
     ----------
-    den_file : TextIO
-        File to parse.
+    pot_file : ~typing.TextIO
+        Open handle to file to parse.
 
     Returns
     -------
-    dict[str, list[int | float]]
-        Parsed data.
+    PotFileInfo
+        Parsed info.
     """
-    return parse_kpt_info(den_file, "density")
+    return cast(PotFileInfo, _parse_kpt_info(pot_file))
+
+
+def parse_den_fmt_file(den_file: TextIO) -> DenFileInfo:
+    """
+    Parse castep .den_fmt file.
+
+    Parameters
+    ----------
+    den_file : ~typing.TextIO
+        Open handle to file to parse.
+
+    Returns
+    -------
+    DenFileInfo
+        Parsed info.
+    """
+    return cast(DenFileInfo, _parse_kpt_info(den_file))
