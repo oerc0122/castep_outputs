f82632cb041454a66e53e8ad70a164074a1d824e
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2025-04-09 11:48:38 +0100

    Add script for predicting generated files from cell/param info.

--- castep_outputs/parsers/cell_param_file_parser.py
+++ castep_outputs/parsers/cell_param_file_parser.py
@@ -1,4 +1,5 @@
 """Parse castep .cell and .param files."""
+
 from __future__ import annotations
 
 import re
@@ -10,9 +11,11 @@ from typing import Any, Literal, TextIO, TypedDict, Union
 
 import castep_outputs.utilities.castep_res as REs
 
+from ..utilities.constants import SHELLS
 from ..utilities.datatypes import (
     AtomIndex,
     MaybeSequence,
+    PSPotStrInfo,
     ThreeByThreeMatrix,
     ThreeVector,
 )
@@ -21,6 +24,7 @@ from ..utilities.utility import (
     atreg_to_index,
     determine_type,
     file_or_path,
+    fix_data_types,
     log_factory,
     normalise_key,
     strip_comments,
@@ -55,12 +59,16 @@ class NonLinearConstraint(TypedDict):
 DevelElem = MaybeSequence[Union[str, float, dict[str, Union[str, float]]]]
 DevelBlock = dict[str, Union[DevelElem, dict[str, DevelElem]]]
 HubbardU = dict[Union[str, AtomIndex], Union[str, dict[str, float]]]
-CellParamData = dict[str, Union[str, float, tuple[float, str],
-                                dict[str, Any], HubbardU, DevelBlock]]
-GeneralBlock = dict[str, Union[
-    list[Union[str, float]],
-    dict[str, MaybeSequence[float]],
-]]
+CellParamData = dict[
+    str, Union[str, float, tuple[float, str], dict[str, Any], HubbardU, DevelBlock],
+]
+GeneralBlock = dict[
+    str,
+    Union[
+        list[Union[str, float]],
+        dict[str, MaybeSequence[float]],
+    ],
+]
 
 
 def _get_block_units(block: Block, default: str) -> str:
@@ -112,10 +120,12 @@ def parse_cell_param_file(cell_param_file: TextIO) -> list[CellParamData]:
         # Strip comments
         line = re.split(r"[#!]", line)[0]
 
-        if block := Block.from_re(line, cell_param_file,
-                                  re.compile(r"^\s*%block ", re.IGNORECASE),
-                                  re.compile(r"^\s*%endblock", re.IGNORECASE)):
-
+        if block := Block.from_re(
+            line,
+            cell_param_file,
+            re.compile(r"^\s*%block ", re.IGNORECASE),
+            re.compile(r"^\s*%endblock", re.IGNORECASE),
+        ):
             block_title = next(block).split()[1].lower()
 
             block = strip_comments(block, remove_inline=True)
@@ -145,6 +155,68 @@ parse_cell_file = parse_cell_param_file
 parse_param_file = parse_cell_param_file
 
 
+def _parse_pspot_string(string: str, *, debug: bool = False) -> PSPotStrInfo:
+    if not (match := REs.PSPOT_RE.search(string)):
+        raise ValueError(f"Attempt to parse {string} as PSPot failed")
+
+    pspot = match.groupdict()
+    projectors = []
+
+    for proj in pspot["proj"].split(":"):
+        if match := REs.PSPOT_PROJ_RE.match(proj):
+            pdict = dict(zip(REs.PSPOT_PROJ_GROUPS, match.groups()))
+        else:
+            raise ValueError("Invalid PSPot string")
+
+        pdict["shell"] = SHELLS[int(pdict["shell"])]
+
+        if not pdict["type"]:
+            pdict["type"] = None
+
+        for prop in ("beta_delta", "de"):
+            if not pdict[prop]:
+                del pdict[prop]
+
+        fix_data_types(pdict, {"orbital": int, "beta_delta": float, "de": float})
+        projectors.append(pdict)
+
+    for prop in ("shell_swp", "shell_swp_end", "opt"):
+        if pspot[prop]:
+            pspot[prop] = pspot[prop].split(",")
+
+    pspot["projectors"] = tuple(projectors)
+    pspot["string"] = string
+    pspot["print"] = bool(pspot["print"])
+
+    if not debug:
+        for prop in (
+            "shell_swp",
+            "shell_swp_end",
+            "local_energy",
+            "poly_fit",
+            "beta_radius",
+            "r_inner",
+            "debug",
+        ):
+            if pspot[prop] is None:
+                del pspot[prop]
+
+    fix_data_types(
+        pspot,
+        {
+            "beta_radius": float,
+            "r_inner": float,
+            "core_radius": float,
+            "coarse": float,
+            "medium": float,
+            "fine": float,
+            "local_channel": int,
+        },
+    )
+
+    return pspot
+
+
 def _parse_devel_code_block(in_block: Block) -> DevelBlock:
     """
     Parse devel_code block to dict.
@@ -170,8 +242,7 @@ def _parse_devel_code_block(in_block: Block) -> DevelBlock:
         block_title = normalise_key(blk.group(1))
         block_str = blk.group(0).split(":")[1]
         block: DevelBlock = {}
-        for par in re.finditer(REs.DEVEL_CODE_VAL_RE, block_str,
-                               re.IGNORECASE | re.MULTILINE):
+        for par in re.finditer(REs.DEVEL_CODE_VAL_RE, block_str, re.IGNORECASE | re.MULTILINE):
             key, val = re.split(r"[:=]", par.group(0))
             key = normalise_key(key)
             typ = determine_type(val)
@@ -254,8 +325,10 @@ def _parse_nonlinear_constraints(block: Block) -> list[NonLinearConstraint]:
         key, line = line.split(maxsplit=1)
         cons: NonLinearConstraint = {
             "key": key,
-            "atoms": {atreg_to_index(match): to_type(match.group("x", "y", "z"), int)
-                      for match in REs.ATOMIC_DATA_3VEC.finditer(line)},
+            "atoms": {
+                atreg_to_index(match): to_type(match.group("x", "y", "z"), int)
+                for match in REs.ATOMIC_DATA_3VEC.finditer(line)
+            },
         }
         accum.append(cons)
 
@@ -328,8 +401,10 @@ def _parse_hubbard_u(block: Block) -> HubbardU:
         if match := re.match(rf"^\s*({REs.ATOM_NAME_RE})\s*(\d+)?", line):
             spec = (match[1], int(match[2])) if match.lastindex == 2 else match[1]
             line = line.replace(match[0], "")
-            accum[spec] = {key: to_type(val, determine_type(val))
-                           for key, val in REs.COLON_SEP_RE.findall(line)}
+            accum[spec] = {
+                key: to_type(val, determine_type(val))
+                for key, val in REs.COLON_SEP_RE.findall(line)
+            }
 
     return accum
 
@@ -362,8 +437,9 @@ def _parse_sedc(block: Block) -> dict[str, dict[str, float]]:
             continue
 
         spec, line = line.strip().split(maxsplit=1)
-        accum[spec] = {key: to_type(val, determine_type(val))
-                       for key, val in REs.COLON_SEP_RE.findall(line)}
+        accum[spec] = {
+            key: to_type(val, determine_type(val)) for key, val in REs.COLON_SEP_RE.findall(line)
+        }
 
     return accum
 
@@ -382,13 +458,40 @@ def _parse_symops(block: Block) -> list[dict[str, ThreeByThreeMatrix | ThreeVect
     list[dict[str, ThreeByThreeMatrix | ThreeVector]]
         Parsed info.
     """
-    tmp = [to_type(numbers, float)
-           for line in block
-           if (numbers := REs.FLOAT_RAT_RE.findall(line))]
+    tmp = [to_type(numbers, float) for line in block if (numbers := REs.FLOAT_RAT_RE.findall(line))]
 
-    return [{"r": tmp[i:i + 3],  # type: ignore
-             "t": tmp[i + 3]}
-            for i in range(0, len(tmp), 4)]
+    return [
+        {
+            "r": tmp[i : i + 3],  # type: ignore
+            "t": tmp[i + 3],
+        }
+        for i in range(0, len(tmp), 4)
+    ]
+
+
+def _parse_species_pot(block: Block) -> dict[str, str | PSPotStrInfo]:
+    """Parse species pot block.
+
+    Parameters
+    ----------
+    block : Block
+        Input block to parse.
+
+    Returns
+    -------
+    dict[str, str | PSPotStrInfo]
+        Processed block if PSPot string, otherwise bare string.
+    """
+    block_data = {}
+    block.remove_bounds(fore=0, back=1)
+
+    for line in block:
+        spec, pot = line.split(maxsplit=1)
+        if REs.PSPOT_RE.search(pot):  # We have pspot definition
+            pot = _parse_pspot_string(pot)
+        block_data[spec] = pot
+
+    return block_data
 
 
 def _parse_general(block: Block) -> GeneralBlock:
@@ -413,8 +516,7 @@ def _parse_general(block: Block) -> GeneralBlock:
         if REs.SPEC_PROP_RE.match(line):
             if isinstance(block_data["data"], list):
                 block_data["data"] = {}
-                typing.cast(dict[str, MaybeSequence[Union[float, str]]],
-                            block_data["data"])
+                typing.cast(dict[str, MaybeSequence[Union[float, str]]], block_data["data"])
 
             spec, val = line.strip().split(maxsplit=1)
             val = to_type(val, determine_type(val))
@@ -440,15 +542,18 @@ _parse_positions_abs = partial(_parse_positions, absolute=True)
 _parse_positions_frac = partial(_parse_positions, absolute=False)
 
 #: Cell/Param subparsers.
-_PARSERS: dict[str, Callable] = {"devel_code": _parse_devel_code_block,
-            "ionic_constraints": _parse_ionic_constraints,
-            "nonlinear_constraints": _parse_nonlinear_constraints,
-            "positions_abs": _parse_positions_abs,
-            "positions_frac": _parse_positions_frac,
-            "positions_abs_intermediate": _parse_positions,
-            "positions_frac_intermediate": _parse_positions,
-            "positions_abs_product": _parse_positions,
-            "positions_frac_product": _parse_positions,
-            "sedc_custom_params": _parse_sedc,
-            "hubbard_u": _parse_hubbard_u,
-            "symmetry_ops": _parse_symops}
+_PARSERS: dict[str, Callable] = {
+    "devel_code": _parse_devel_code_block,
+    "ionic_constraints": _parse_ionic_constraints,
+    "nonlinear_constraints": _parse_nonlinear_constraints,
+    "positions_abs": _parse_positions_abs,
+    "positions_frac": _parse_positions_frac,
+    "positions_abs_intermediate": _parse_positions,
+    "positions_frac_intermediate": _parse_positions,
+    "positions_abs_product": _parse_positions,
+    "positions_frac_product": _parse_positions,
+    "species_pot": _parse_species_pot,
+    "sedc_custom_params": _parse_sedc,
+    "hubbard_u": _parse_hubbard_u,
+    "symmetry_ops": _parse_symops,
+}
---
+++ castep_outputs/scripts/__init__.py
@@ -0,0 +1,4 @@
+"""Collection of basic scripts using castep_outputs.
+
+For more advanced scripts, see ``castep_outputs_tools``
+"""
