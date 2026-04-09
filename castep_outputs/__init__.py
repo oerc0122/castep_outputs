3da64e89dcf16d452a8c4168f7ee4b3df083ff58
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2026-04-09 10:01:32 +0100

    Move `MDGeomParser` into castep_outputs (#218)

--- castep_outputs/__init__.py
+++ castep_outputs/__init__.py
@@ -3,7 +3,7 @@
 __author__ = "Jacob Wilkins"
 __version__ = "0.2.0"
 
-# pylint: disable=unused-import
-
 from .cli.castep_outputs_main import parse_single  # noqa: F401
 from .parsers import *  # noqa: F403
+from .tools import MDGeomParser as MDGeomParser
+from .tools import get_generated_files as get_generated_files
